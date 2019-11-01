0b83f3e88aa7db8fb03cf30aeb1f749fb8aad39d
David Almeida <davidalmeida2@Knights.ucf.edu>
2019-11-01 14:22:45 -0400

    Initial Commit from Windows Machine

---
+++ CoL.py
@@ -0,0 +1,2 @@
+print('This program is designed to help predict cost of living expenses.')
+print('Users will first select an option from the menu,\nthen provide information needed to perform calculations')
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# smallPrograms
+##Some basic Python Programs
+---------------------------------------
+This was meant to explore some python coding for very basic programs.
